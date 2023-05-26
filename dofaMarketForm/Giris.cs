825ac357f3f2f41e814488caa8d2e22555ca3ee8
DogukanGercek <111412549+DogukanGercek@users.noreply.github.com>
2023-05-26 16:46:47 +0300

    güncellermeler yapıldı
    amah ata var düzeltilmedi

--- dofaMarketForm/Giris.cs
+++ dofaMarketForm/Giris.cs
@@ -1,4 +1,8 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+
 namespace dofaMarketForm
 {
     public partial class Giris : Form
@@ -7,6 +11,7 @@ namespace dofaMarketForm
         {
             InitializeComponent();
         }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-29SJKEI\SQLEXPRESS;Initial Catalog=C:\USERS\NDOGU\DOCUMENTS\DOFAMARKETDB.MDF;Integrated Security=True");
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -19,6 +24,55 @@ namespace dofaMarketForm
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            String username, userPass;
+
+            username = txt_username.Text;
+            userPass = txt_pass.Text;
+            try
+            {
+                String queery = "SELECT * FROM auth WHERE username = '"+txt_username.Text+ "' AND  password = '"+txt_pass.Text+"'";
+                SqlDataAdapter sda = new SqlDataAdapter(queery, baglanti);
+
+                DataTable dbtable = new DataTable();
+                sda.Fill(dbtable);
+
+                if (dbtable.Rows.Count > 0)
+                {
+                    username = txt_username.Text;
+                    userPass = txt_pass.Text;
+                    //sonraki acilcak sayfa
+                    Calisan form2 = new Calisan();
+                    form2.Show();
+                    this.Hide();
+
+                }
+                else {
+                    MessageBox.Show("Invalid login details", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt_username.Clear();
+                    txt_pass.Clear();
+
+                    txt_username.Focus();
+                      }
+
+                
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
